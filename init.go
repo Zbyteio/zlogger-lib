41776029f9d6f09aaeb83ef11887a35e09517375
sourabhmandal <19mandal97@gmail.com>
2022-11-23 13:09:39 +0530

    create initilizer

--- init.go
+++ init.go
@@ -4,25 +4,28 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-
-
-var (
+type ZLoggers struct {
 	LoggerEnv LogEnvironment
 	ZAppLogger  AppLogger
 	ZGinLogger GinLogger
 	ZGormLogger GormLogger
-)
-// New creates a new logger using the default configuration.
-func InitLoggers(systemEnv LogEnvironment) {
-	LoggerEnv = systemEnv
+}
 
-	if LoggerEnv == Local {
-		ZAppLogger = newDebugZlogger()
+// New creates a new logger using the default configuration.
+func InitLoggers(systemEnv LogEnvironment) ZLoggers {
+	var zlogger ZLoggers = ZLoggers{}
+
+	zlogger.LoggerEnv = systemEnv
+
+	if zlogger.LoggerEnv == Local {
+		zlogger.ZAppLogger = newDebugZlogger()
 	} else {
-		ZAppLogger = newZlogger()
+		zlogger.ZAppLogger = newZlogger()
 	}
 
-	ZGinLogger = newGinLogger(ZAppLogger)
-	gin.DebugPrintRouteFunc = ZGinLogger.ginDebugLogger
-	ZGormLogger = newGormlogger(ZAppLogger)
+	zlogger.ZGinLogger = newGinLogger(zlogger.ZAppLogger)
+	gin.DebugPrintRouteFunc = zlogger.ZGinLogger.ginDebugLogger
+	zlogger.ZGormLogger = newGormlogger(zlogger.ZAppLogger)
+
+	return zlogger
 }
