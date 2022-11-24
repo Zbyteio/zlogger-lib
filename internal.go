57f02ef783de81083bccf28c46f5fca283aaa7d9
sourabhmandal <19mandal97@gmail.com>
2022-11-24 13:34:29 +0530

    modularise color codes

--- internal.go
+++ internal.go
@@ -2,55 +2,11 @@
 package zlogger
 
 import (
-	"strconv"
-	"time"
 	"unicode"
 )
 
 // ---------------------- INTERNAL --------------------------------------
 
-func colorifySatusCode(statusCode int) string {
-	var statusCodeString string = strconv.Itoa(statusCode)
-	if statusCode >= 500 {
-		return fgRed.coloredString(statusCodeString)
-	} else if statusCode >= 400 {
-		return fgYellow.coloredString(statusCodeString)
-	} else if statusCode >= 300 {
-		return fgCyan.coloredString(statusCodeString)
-	} else if statusCode >= 200 {
-		return fgGreen.coloredString(statusCodeString)
-	}
-	// default value
-	return fgWhite.coloredString(statusCodeString)
-}
-
-func colorifyRequestMethod(methodName string) string {
-	switch methodName {
-	case "GET":
-		return bgGreen.coloredString(methodName)
-	case "POST":
-		return bgYellow.coloredString(methodName)
-	case "PUT":
-		return bgBlue.coloredString(methodName)
-	case "DELETE":
-		return bgRed.coloredString(methodName)
-	case "OPTION":
-		return bgCyan.coloredString(methodName)
-	case "PATCH":
-		return bgMagenta.coloredString(methodName)
-	default:
-		return bgWhite.coloredString(methodName)
-	}
-}
-
-func colorifyRequestLatency(latency time.Duration) string {
-	if latency < time.Second {
-		return fgGreen.coloredString(latency.String())
-	} else if latency < time.Second*2 {
-		return fgYellow.coloredString(latency.String())
-	}
-	return fgRed.coloredString(latency.String())
-}
 
 
 
